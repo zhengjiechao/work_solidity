f34141a77c0fb5976cd776f9983622e2512f7cfe
zhengjiechao <150@qq.com>
2025-09-27 09:19:39 +0800

    完善任务2

--- task2/ERC721.sol
+++ task2/ERC721.sol
@@ -1,27 +1,35 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-contract ERC721{
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-    string public name;
-    string public symbol;
+contract MyERC721 is ERC721, ERC721URIStorage {
+    uint256 private _nextTokenId;
 
-    constructor(string memory _name,string memory _symbol){
-        name = _name;
-        symbol = _symbol;
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC721(name_, symbol_) {
+        _nextTokenId = 0;
     }
 
-    function mint()public {}
+    function mintNFT(address recipient, string memory _tokenURI) public {
+        uint256 currentTokenId = _nextTokenId;
+        _nextTokenId++;
+        _safeMint(recipient, currentTokenId);
+        _setTokenURI(currentTokenId, _tokenURI);
+    }
 
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
 
-
-
-
-
-
-
-
-
-
-
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
 }
